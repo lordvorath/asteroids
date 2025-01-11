a12b95d856f62b9b9550b8003629657571869499
Stefano Trentin <4kill3@gmail.com>
2025-01-11 11:31:52 +0100

    Ch2 L8

--- main.py
+++ main.py
@@ -15,16 +15,29 @@ def main():
     
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
-    player = Player(x, y)
 
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+    player = Player(x, y)
+    
+    # Game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
-        player.draw(screen)
 
+        for thing in updateable:
+            thing.update(dt)
+
+        screen.fill((0,0,0))
+
+        for thing in drawable:
+            thing.draw(screen)
+        
         pygame.display.flip()
+
+        # limit frame rate to 60
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
