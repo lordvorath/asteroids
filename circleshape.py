217f07cf3119081ad8410c85d4f6ba5afbba6588
Stefano Trentin <4kill3@gmail.com>
2025-03-29 13:10:44 +0100

    Added player

--- circleshape.py
+++ circleshape.py
@@ -20,9 +20,3 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
-
-    def collide(self, other):
-        distance = self.position.distance_to(other.position)
-        if distance > self.radius + other.radius:
-            return False
-        return True
--- main.py
+++ main.py
@@ -1,23 +1,27 @@
+from player import *
 from constants import *
 import pygame
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     clock = pygame.time.Clock()
     dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill(color="black")
+        player.draw(screen)
         pygame.display.flip()
+
         dt = clock.tick(60) / 1000
 
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    
 
 if __name__ == "__main__":
     main()
